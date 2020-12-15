79a46a029504aa3e71acadf4b301ccdb8c087b3c
antonioramsan <antonio.ramirez.santander@gmail.com>
2020-12-15 00:32:54 -0600

    json pretty print

--- TSTrivialAPI/Startup.cs
+++ TSTrivialAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace TSTrivialAPI
 {
@@ -25,7 +26,10 @@ namespace TSTrivialAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(
+                options => {
+                    options.SerializerSettings.Formatting = Formatting.Indented;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
